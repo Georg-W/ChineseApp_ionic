9ce2b3a88107583552e630fd36a86569c3e80952
Georg-W <georg.westner@googlemail.com>
2016-07-12 19:05:23 +0200

    immersion finished

--- myApp/www/js/ImmersionController.js
+++ myApp/www/js/ImmersionController.js
@@ -1,39 +1,76 @@
 /**
  * Created by Georg on 02.06.2016.
  */
-(function(){
-    'use strict';
+(function () {
+  'use strict';
 
 
-    angular
-        .module('myApp.controllers')
-        .controller('immersionController',['$scope', 'dataservice', function($scope, dataservice) {
+  angular
+    .module('myApp.controllers')
+    .controller('immersionController', ['$scope', 'dataservice', function ($scope, dataservice) {
 
-          var youtubeChannelsShow = false;
-          var moviesShow = false;
-          var comedyShow = false;
-          var musicShow = false;
-          var talksShow = false;
-          var podcastsShow = false;
-          var booksShow = false;
-          var cultureShow = false;
-          var blogsShow = false;
-          var martialShow = false;
-          var philosophyShow = false;
-          var otherShow = false;
+      $scope.youtubeChannelsShow = false;
+      $scope.moviesShow = false;
+      $scope.comedyShow = false;
+      $scope.musicShow = false;
+      $scope.talksShow = false;
+      $scope.podcastsShow = false;
+      $scope.booksShow = false;
+      $scope.cultureShow = false;
+      $scope.blogsShow = false;
+      $scope.martialShow = false;
+      $scope.philosophyShow = false;
+      $scope.otherShow = false;
 
-            getLinks();
+      getLinks();
 
-            function getLinks() {
-                dataservice.getLinks()
-                    .then(function (response) {
-                        $scope.Links = response.data;
-                      console.log($scope.Links[1]);
-                    }, function (error) {
-                        $scope.status = 'Unable to load customer data: ' + error.message;
-                    });
-            }
+      function getLinks() {
+        dataservice.getLinks()
+          .then(function (response) {
+            $scope.Links = response.data;
+          }, function (error) {
+            $scope.status = 'Unable to load customer data: ' + error.message;
+          });
+      }
+
+      $scope.youtubeChannelsChange = function () {
+        $scope.youtubeChannelsShow = !$scope.youtubeChannelsShow;
+      };
+      $scope.moviesChange = function () {
+        $scope.moviesShow = !$scope.moviesShow;
+      };
+      $scope.comedyChange = function () {
+        $scope.comedyShow = !$scope.comedyShow;
+      };
+      $scope.musicChange = function () {
+        $scope.musicShow = !$scope.musicShow;
+      };
+      $scope.talksChange = function () {
+        $scope.talksShow = !$scope.talksShow;
+      };
+      $scope.podcastsChange = function () {
+        $scope.podcastsShow = !$scope.podcastsShow;
+      };
+      $scope.booksChange = function () {
+        $scope.booksShow = !$scope.booksShow;
+      };
+      $scope.cultureChange = function () {
+        $scope.cultureShow = !$scope.cultureShow;
+      };
+      $scope.blogsChange = function () {
+        $scope.blogsShow = !$scope.blogsShow;
+      };
+      $scope.martialChange = function () {
+        $scope.martialShow = !$scope.martialShow;
+      };
+      $scope.philosophyChange = function () {
+        $scope.philosophyShow = !$scope.philosophyShow;
+      };
+      $scope.otherChange = function () {
+        $scope.otherShow = !$scope.otherShow;
+      };
 
 
-        }])
+
+    }])
 })();
