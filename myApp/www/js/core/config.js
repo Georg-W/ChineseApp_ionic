3563464ff0210345626996629c2ff529666f86f2
Georg-W <georg.westner@googlemail.com>
2016-08-15 22:08:52 +0200

    ui router partially working

--- myApp/www/js/core/config.js
+++ myApp/www/js/core/config.js
@@ -4,115 +4,125 @@
 
 (function(){
 
-        'use strict';
+  'use strict';
 
 
 
-        var core = angular.module('myApp.core')////////////////Getter for core.module.js (imports dependencies)
+  var core = angular.module('myApp.core')////////////////Getter for core.module.js (imports dependencies)
 
 
-  .config(function($stateProvider, $urlRouterProvider, $translateProvider) {
+    .config(function($stateProvider, $urlRouterProvider, $translateProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-      .state('app', {
-        url: '/app',
-        templateUrl: function() {
-          if (ionic.Platform.isAndroid()) {
-            return  'templates/android/menu.html';
-          }
-          return "templates/desktop/menu.html";
-        },
-        abstract:true
-      })
+        .state('app', {
+          url: '/app',
+          templateUrl: 'templates/android/menu.html',
+          abstract:true
+        })
 
-      .state('app.quiz', {
-        url: '/quiz',
-        views: {
-          'sidemenu': {
-            templateUrl: function() {
-              if (ionic.Platform.isAndroid()) {
-                return  'templates/android/quiz.html';
-              }
-              return "templates/desktop/quiz.html";
-            },
-            controller: 'quizController'
-          }
-        }
-      })
-
-      .state('app.flashcards', {
-        url: '/flashcards',
-        views: {
-          'sidemenu': {
-            templateUrl: function() {
-            if (ionic.Platform.isAndroid()) {
-              return  'templates/android/flashcards.html';
-            }
-            return "templates/desktop/flashcards.html";
-            },
-            controller: 'flashCardController'
-          }
-        }
-      })
-
-
-      .state('app.settings', {
-        url: '/settings',
-        views: {
-          'sidemenu': {
-            templateUrl: function() {
-              if (ionic.Platform.isAndroid()) {
-                return  'templates/android/settings.html';
-              }
-              return "templates/desktop/settings.html";
-            },
-            controller: 'navController'
-          }
-        }
-      })
-
-      .state('app.about', {
-        url: '/about',
-        views: {
-          'sidemenu': {
-            templateUrl: function() {
-              if (ionic.Platform.isAndroid()) {
-                return  'templates/android/about.html';
-              }
-              return "templates/desktop/about.html";
+        .state('app.quiz', {
+          url: '/quiz',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/android/quiz.html',
+              controller: 'quizController'
             }
           }
-        }
-      })
+        })
 
-      .state('app.feedback', {
-        url: '/feedback',
-        views: {
-          'sidemenu': {
-            templateUrl: 'templates/feedback.html'
+        .state('app.flashcards', {
+          url: '/flashcards',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/android/flashcards.html',
+              controller: 'flashCardController'
+            }
           }
-        }
-      });
-
-    $urlRouterProvider.otherwise('/app/flashcards');
-
-    $translateProvider
-      .preferredLanguage('en')
-
-      .useStaticFilesLoader({
-        prefix:'./lang/',
-        suffix:'.json'
-
-      })
-      .fallbackLanguage('en')
-      .useSanitizeValueStrategy('escape');
+        })
 
 
-  });
+        .state('app.settings', {
+          url: '/settings',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/android/settings.html',
+              controller: 'navController'
+            }
+          }
+        })
+
+        .state('app.about', {
+          url: '/about',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/android/about.html'
+            }
+          }
+        })
+
+        .state('app.feedback', {
+          url: '/feedback',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/android/feedback.html'
+            }
+          }
+        })
+
+        .state('app.f', {
+          url: '/f',
+          views: {
+            'sidemenu': {
+              template: "<h1>HELLO!</h1>"
+            }
+          }
+        })
+        .state('app.q', {
+          url: '/q',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/desktop/quiz.html'
+            }
+          }
+        })
+        .state('app.a', {
+          url: '/a',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/desktop/about.html'
+            }
+          }
+        })
+        .state('app.fe', {
+          url: '/fe',
+          views: {
+            'sidemenu': {
+              templateUrl: 'templates/desktop/feedback.html'
+            }
+          }
+        });
+
+
+
+      $urlRouterProvider.otherwise('/app/f');
+
+      $translateProvider
+        .preferredLanguage('en')
+
+        .useStaticFilesLoader({
+          prefix:'./lang/',
+          suffix:'.json'
+
+        })
+        .fallbackLanguage('en')
+        .useSanitizeValueStrategy('escape');
+
+
+    });
 
 })();
